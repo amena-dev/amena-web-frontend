f7013558073b1670ec78205382894db5dee02b59
上原雄大 <uehr@MacBook-Pro.local>
2021-02-10 02:13:43 +0900

    udpate: header modeでlogoに色がつくように

--- src/components/molecules/header.tsx
+++ src/components/molecules/header.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import '../../assets/css/header.scss'
-import Logo from '../../assets/img/amena-title-logo.svg'
-
-function convertRemToPixels(rem: number): number {
-    return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
-}
+import WhiteLogo from '../../assets/img/amena-title-logo.svg'
+import ColorLogo from '../../assets/img/amena-title-logo-color.svg'
 
 class Header extends React.Component {
     componentDidMount() {
@@ -16,24 +13,27 @@ class Header extends React.Component {
     }
 
     onScrolling() {
-        const background = document.getElementById("background-video")
+        const logo_class_name = "logo"
+        const is_header_mode = window.innerHeight >= window.pageYOffset
+        console.log(window.innerHeight + " " + window.pageYOffset)
 
-        if(background) {
-            if(window.innerHeight - convertRemToPixels(5) >= window.pageYOffset) {
-                background.classList.remove("expand")
+        Array.from(document.getElementsByClassName(logo_class_name)).forEach(logo => {
+            if(is_header_mode) {
+                logo.classList.remove("header-mode")
                 console.log("not expand")
             }else{
-                background.classList.add("expand")
+                logo.classList.add("header-mode")
                 console.log("expand")
             }
-        }
+        });
     }
 
     render() {
         return (
             <div id="header">
                 <video src={"/amena-web-background.mp4"} id="background-video" autoPlay loop muted>yay :)</video>
-                <img src={Logo} alt="logo" id="logo" />
+                <img src={WhiteLogo} alt="logo" id="logo-white" className="logo" />
+                <img src={ColorLogo} alt="logo" id="logo-color" className="logo"/>
                 <p id="description">3D Photo Generator.</p>
             </div>
         )
